e6a163ba083faee8e2fc6f73c1626625d6ab6b87
tigerweirdo <temmuzcetiner@gmail.com>
2023-06-18 21:36:24 +0200

    css düzenlendi-2

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
-import MenuItem from './MenuItem/Menuitem';
 import Navbar from './Navbar/Navbar';
 
 export {
   SubHeading,
   Newsletter,
-  MenuItem,
   Navbar,
 };
