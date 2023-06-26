6495eb986c5cf0e310af65ecbbd97adb2e352bd4
tigerweirdo <temmuzcetiner@gmail.com>
2023-06-26 02:12:04 +0200

    gsap kaldırıldı.

--- src/components/sign/index.js
+++ src/components/sign/index.js
@@ -1,59 +1,34 @@
-import { useEffect, useRef, useState } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import React, { useEffect, useRef, useState } from 'react'
 import './index.css'
 import NewComponent from './sign'
 
 const Logo = () => {
   const bgRef = useRef()
-  const outlineLogoRef = useRef()
-  const solidLogoRef = useRef()
   const [key, setKey] = useState(0)
-  const [loadComponent, setLoadComponent] = useState(false) // Yeni durumu ekledik.
+  const [loadComponent, setLoadComponent] = useState(false)
 
   const handleMouseEnter = () => {
-    setKey(prevKey => prevKey + 1); // Mouse üzerine gelindiğinde anahtarı artır
+    setKey(prevKey => prevKey + 1); 
   }
 
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 1,
-        duration: 15,
-      })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 5,
-        duration: 4,
-      }
-    )
-    setTimeout(() => { // Yeni bileşeni yükle
+    bgRef.current.style.opacity = 1
+    setTimeout(() => { 
       setLoadComponent(true);
-    }, 1000); // Burada 1 saniye gecikme ekliyoruz
+    }, 1000); 
   }, [])
-  
 
   return (
     <div className="logo-container" ref={bgRef}>
       <img
         className="solid-logo"
-        ref={solidLogoRef}
+        onAnimationEnd={() => {
+          bgRef.current.style.opacity = 0
+        }}
+        style={{ animation: 'fade-in 5s ease-in-out' }}
       />
       <div onMouseEnter={handleMouseEnter}>
-        {loadComponent && <NewComponent key={key} />} {/* Yüklemeyi burada kontrol ediyoruz */}
+        {loadComponent && <NewComponent key={key} />}
       </div>
     </div>
   )
