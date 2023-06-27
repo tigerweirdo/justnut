9daa122ad011a60f19e0c36894430b901e794ce9
tigerweirdo <temmuzcetiner@gmail.com>
2023-06-27 18:56:55 +0300

    sincap ve back-end halledildi.

---
+++ back-end/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // CORS'un uygulanmasını sağlar.
+
+const port = 3000;
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const dbname = process.env.MONGODB_DBNAME;
+
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.cutmm6z.mongodb.net/${dbname}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// E-posta adreslerini depolamak için bir Mongoose modeli tanımla
+const Email = mongoose.model('Email', new mongoose.Schema({
+  address: String
+}));
+
+app.use(express.json());
+
+app.post('/api/newsletter', async (req, res) => {
+  const email = req.body.email;
+
+  if (email) {
+    console.log(`E-posta alındı: ${email}`);
+
+    // E-posta adresini veritabanına kaydet
+    const newEmail = new Email({ address: email });
+    await newEmail.save();
+
+    res.status(200).send('E-posta alındı ve veritabanına kaydedildi!');
+  } else {
+    res.status(400).send('E-posta gönderilmedi!');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu http://localhost:${port} adresinde çalışıyor`);
+});
--- front-end/src/components/Footer/Newsletter.jsx
+++ front-end/src/components/Footer/Newsletter.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
-
+import axios from 'axios'; // axios import etmeyi unutmayın!
 import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => {
   const [isClicked, setIsClicked] = useState(false);
+  const [email, setEmail] = useState('');
 
   const handleClick = () => {
     setIsClicked(true);
     setTimeout(() => {
         setIsClicked(false);
     },1000); 
-};
 
+    // E-posta adresini sunucuya gönder
+    axios.post('http://localhost:3000/api/newsletter', { email })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
 
   return (
     <div className="app__newsletter">
@@ -22,12 +35,16 @@ const Newsletter = () => {
         <p className="p__opensans"></p>
       </div>
       <div className="app__newsletter-input flex__center">
-        <input type="email" placeholder="Mail adresinizi giriniz" />
+        <input 
+          type="email" 
+          placeholder="Mail adresinizi giriniz" 
+          value={email}
+          onChange={handleEmailChange} 
+        />
         <div className="wrap">
           <button 
             className={`button ${isClicked ? 'clicked' : ''}`} 
             onClick={handleClick}
-           
           >
             Submit
           </button>
