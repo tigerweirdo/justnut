931090acdb00e8ecb659f112afad388ffca4f103
tigerweirdo <temmuzcetiner@gmail.com>
2023-06-27 10:38:47 +0300

    sincap animasyonu tamamlandı.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
   import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
   import { AboutUs, FindUs, Footer, Gallery, Header, Intro, SpecialMenu, Footer2, Spacer } from './container';
-  import { Navbar } from './components';
+  import { Navbar, Animations } from './components';
   import Products from './pages/products/products'; // components should be PascalCase
   import './App.css';
 
@@ -11,6 +11,7 @@
     <>
       <Header />
       <AboutUs />
+      <Animations/>
       <SpecialMenu />
       <Intro />
       <Gallery />
